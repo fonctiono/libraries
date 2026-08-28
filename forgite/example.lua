9b7845d219e96ff159607ac18c86a847b87d7156
! client <137990076+fonctiono@users.noreply.github.com>
2026-08-28 15:13:28 +0200

    Create example.lua

---
+++ forgite/example.lua
@@ -0,0 +1,41 @@
+local Library = loadstring(game:HttpGet("ex"))()
+local Window = Library:CreateWindow("ngalight | da.cli3nt")
+
+local Section1 = Window:CreateSection("Section 1")
+local Tab1 = Section1:CreateTab("Tab 1")
+local Tab2 = Section1:CreateTab("Tab 2")
+
+
+local SettingsSection = Window:CreateSection("Settings")
+local SettingsTab = SettingsSection:CreateTab("GUI Settings")
+
+
+Tab1:AddToggle("Toggle", false, function(state)
+    print(state)
+end)
+Tab1:AddSlider("Slider", 10, 500, 120, function(val)
+    print(val)
+end)
+Tab2:AddColorPicker("Color Picker", Color3.fromRGB(255, 255, 255), function(col)
+    print(tostring(col))
+end)
+Tab2:AddDropdown("Dropdown", {"Option 1", "Option 2", "Option 3"}, "Option 2", function(selected)
+    print(selected)
+end)
+Tab2:AddKeybind("Keybind", Enum.KeyCode.X, function(key)
+    print(tostring(key))
+end)
+
+
+SettingsTab:AddColorPicker("Color", Library.AccentColor, function(col)
+    Library:SetAccentColor(col)
+end)
+SettingsTab:AddKeybind("Menu", Library.ToggleKeybind, function(key)
+    Library.ToggleKeybind = key
+end)
+SettingsTab:AddSlider("Window Width", 400, 800, 500, function(width)
+    Window:SetSize(width, Window.WindowFrame.Size.Y.Offset)
+end)
+SettingsTab:AddSlider("Window Height", 400, 900, 626, function(height)
+    Window:SetSize(Window.WindowFrame.Size.X.Offset, height)
+end)
